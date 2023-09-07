ac08e0b5470921f442dfa21519966561b60ae0bc
Salustiano-RoblesTeran <saluroblesteran@gmail.com>
2023-09-07 11:00:25 -0300

    Iniciamos proyecto, empezamos el backend

---
+++ backend/src/routes.js
@@ -0,0 +1,11 @@
+const {Router} = require('express');
+
+const routes = Router();
+const UserController = require('./controllers/UserController');
+
+routes.post('/users', UserController.store);
+
+
+
+
+module.exports = routes;
